31a336884eb72ebd5b7ffce90af13dd8113419e0
Yerkebulan <erke.zhumagali@gmail.com>
2017-10-07 10:41:17 +0600

    Contest[3]

--- Contest[3]/ProblemB.cpp
+++ Contest[3]/ProblemB.cpp
@@ -1,28 +1,25 @@
 	#include <iostream>
 using namespace std;
 
-	long long int fact(int N)
-	{
-    if(N < 0)
-        return 0;
-    if (N == 0)     
-        return 1; 	
-    else 
-        return N * fact(N - 1); 
+#define ll long long 
+
+ll a, b;
+int main(){  
+	//freopen("in.in", "r", stdin);
+	//freopen("out.out", "w", stdout);
+	cin >> a >> b;
+	if(b >= a + 10){
+		cout << 0 << endl;
+		return 0;
 	}
-
-	int main() {
-
-   	int a,b;
-    cin >>a >> b;
-    long long int b1 = fact(b);
-    long long int a1 = fact(a);
-
-   	int res = (b1 / a1) % 10;
-    cout << res <<endl;
-
-
-
-
+	else{
+		ll cur = 1;
+		for(ll t = a + 1; t <= b; ++t){
+			ll x = t % 10;
+			cur *= x;
+			cur %= 10;
+		}
+		cout << cur << endl;
+	}
     return 0;
-    }
+}                            
